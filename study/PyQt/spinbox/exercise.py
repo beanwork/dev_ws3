f2486b350111054b9afaedef8d2656746fd46054
beanwork <isumin138@gmail.com>
2023-10-16 20:07:23 +0900

    failed resize picture

--- study/PyQt/spinbox/exercise.py
+++ study/PyQt/spinbox/exercise.py
@@ -3,8 +3,9 @@ import urllib.request
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
 
-from_class = uic.loadUiType("spinbox.ui")[0]
+from_class = uic.loadUiType("exercise.ui")[0]
 
 class WindowClass(QMainWindow, from_class):
 
@@ -12,40 +13,41 @@ class WindowClass(QMainWindow, from_class):
         super().__init__()
         self.setupUi(self)
 
-        self.height.setRange(0, self.label_5.height())
-        self.height.setSingleStep(1)
+        self.height_bar.setRange(100, self.label_5.height())
+        self.height_bar.setSingleStep(1)
 
-        self.width.setRange(0, self.label_5.width())
-        self.width.setSingleStep(1)
+        self.width_bar.setRange(100, self.label_5.width())
+        self.width_bar.setSingleStep(1)
 
         self.pushButton_2.clicked.connect(self.savefile)
         self.pushButton_3.clicked.connect(self.openfile)
 
-        self.height.valueChanged.connect(self.resize)
-        self.width.valueChanged.connect(self.resize)
+        self.height_bar.valueChanged.connect(self.resize_picture)
+        self.width_bar.valueChanged.connect(self.resize_picture)
 
-        url = 'https://imageio.forbes.com/specials-images/imageserve/61b1f75e9bdd78e1c08fdd64/A-funny-labrador-dog-with-a-curiously-placed-bubble-in-its-behind-/0x0.jpg?format=jpg&crop=922,956,x0,y279,safe&width=1440'
-        image = urllib.request.urlopen(url).read()
-
+        # url = 'https://imageio.forbes.com/specials-images/imageserve/61b1f75e9bdd78e1c08fdd64/A-funny-labrador-dog-with-a-curiously-placed-bubble-in-its-behind-/0x0.jpg?format=jpg&crop=922,956,x0,y279,safe&width=1440'
+        # image = urllib.request.urlopen(url).read()
+        
         self.pixmap = QPixmap()
-        self.pixmap.loadFromData(image)
-        ## resize image to fit label size
-        self.pixmap = self.pixmap.scaled(self.label_5.width(), self.label_5.height())
-        self.label_5.setPixmap(self.pixmap)
 
-    def changeWidth(self):
-        actualValue = self.width.value()
-        self.pixmap = self.pixmap.scaled(actualValue, actualValue)
-        self.label_5.setPixmap(self.pixmap)
 
-    def changeHeight(self):
-        actualValue = self.height.value()
-        self.pixmap = self.pixmap.scaled(self.label_5.height(), actualValue)
-        self.label_5.setPixmap(self.pixmap)
+    # def changeWidth(self):
+    #     width_value = self.width_bar.value()
+    #     self.pixmap = self.pixmap.scaledToWidth(width_value)
+    #     self.label_5.setPixmap(self.pixmap)
+        
+
+    # def changeHeight(self):
+    #     height_value = self.height_bar.value()
+    #     self.pixmap = self.pixmap.scaledToHeight(height_value)
+    #     self.label_5.setPixmap(self.pixmap)
     
-    def resize(self):
-        actualValue = self.height.value()
-        self.pixmap = self.pixmap.scaled(self.label_5.height(), actualValue)
+    def resize_picture(self):
+        height_value = self.height_bar.value()
+        width_value = self.width_bar.value()
+        
+        self.pixmap = self.pixmap.scaled(width_value, height_value)
+        self.label_5.resize(self.pixmap.width(), self.pixmap.height())
         self.label_5.setPixmap(self.pixmap)
 
     def savefile(self):
@@ -58,9 +60,11 @@ class WindowClass(QMainWindow, from_class):
                 
     def openfile(self):
         path_name = QFileDialog.getOpenFileName(self, 'open file', './')
+
+        ## resize image to fit label size
         if path_name[0]:
             self.pixmap.load(path_name[0])
-            self.pixmap = self.pixmap.scaled(self.label_5.width(), self.label_5.height())
+            self.pixmap = self.pixmap.scaled(self.label_5.width(), self.label_5.height(),)
             self.label_5.setPixmap(self.pixmap)                
     
        
