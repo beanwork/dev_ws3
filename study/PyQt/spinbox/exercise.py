52f28b318a784c998ffe680d46dd8d5b969f157c
beanwork <isumin138@gmail.com>
2023-10-16 18:01:18 +0900

    fail resize image

--- study/PyQt/spinbox/exercise.py
+++ study/PyQt/spinbox/exercise.py
@@ -26,8 +26,8 @@ class WindowClass(QMainWindow, from_class):
         self.pushButton_2.clicked.connect(self.savefile)
         self.pushButton_3.clicked.connect(self.openfile)
 
-        self.spinBox.valueChanged.connect(self.changeSpinbox)
-        self.horizontalSlider.valueChanged.connect(self.changeSlider)
+        self.spinBox.valueChanged.connect(self.changeSpinbox_and_changePixmapHeight)
+        self.horizontalSlider.valueChanged.connect(self.changeSlider_and_changePixmapWidth)
 
         url = 'https://imageio.forbes.com/specials-images/imageserve/61b1f75e9bdd78e1c08fdd64/A-funny-labrador-dog-with-a-curiously-placed-bubble-in-its-behind-/0x0.jpg?format=jpg&crop=922,956,x0,y279,safe&width=1440'
         image = urllib.request.urlopen(url).read()
@@ -49,15 +49,15 @@ class WindowClass(QMainWindow, from_class):
         self.horizontalSlider.setRange(int(min), int(max))
         self.horizontalSlider.setSingleStep(int(step))
     
-    def changeSlider(self):
+    def changeSlider_and_changePixmapWidth(self):
         actualValue = self.horizontalSlider.value()
-        self.label_4.setText(str(actualValue))
-        self.spinBox.setValue(actualValue)
+        self.pixmap = self.pixmap.scaled(actualValue, self.label_5.height())
+        self.label_5.setPixmap(self.pixmap)
 
-    def changeSpinbox(self):
+    def changeSpinbox_and_changePixmapHeight(self):
         actualValue = self.spinBox.value()
-        self.label_4.setText(str(actualValue))
-        self.horizontalSlider.setValue(actualValue)
+        self.pixmap = self.pixmap.scaled(self.label_width(), actualValue)
+        self.label_5.setPixmap(self.pixmap)
 
     def savefile(self):
         
@@ -65,11 +65,10 @@ class WindowClass(QMainWindow, from_class):
         
         if path_name[0]:
             self.pixmap.save(path_name[0])
-            print("file save : ", path_name)
+    
                 
     def openfile(self):
         path_name = QFileDialog.getOpenFileName(self, 'open file', './')
-        print(path_name)
         if path_name[0]:
             self.pixmap.load(path_name[0])
             self.pixmap = self.pixmap.scaled(self.label_5.width(), self.label_5.height())
