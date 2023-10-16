de4bd77f66bab58e8b19812590265d31939c9ca8
beanwork <isumin138@gmail.com>
2023-10-16 10:28:23 +0900

    add .py

---
+++ study/PyQt/combobox/combo.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+
+from_class = uic.loadUiType("combobox.ui")[0]
+
+class WindowClass(QMainWindow, from_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        for year in range(1900, 2023):
+            self.comboBox.addItem(str(year))
+
+        for month in range(1, 13):
+            self.comboBox_2.addItem(str(month))
+        
+        for day in range(1, 32):
+            self.comboBox_3.addItem(str(day))
+
+        self.comboBox.setCurrentText(str(1990))
+        self.comboBox_3.currentIndexChanged.connect(self.printBirthday)
+    
+    def printBirthday(self):
+        year = self.comboBox.currentText()
+        month = self.comboBox_2.currentText()
+        day = self.comboBox_3.currentText()
+
+        self.lineEdit.setText(year+month.zfill(2)+day.zfill(2))
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mywindows = WindowClass()
+    mywindows.show()
+    sys.exit(app.exec_())
