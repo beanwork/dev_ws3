c6b7c012b55cf1a63479c6bac1115b89d302c3f4
beanwork <isumin138@gmail.com>
2023-10-16 20:30:05 +0900

    error

--- study/PyQt/spinbox/exercise.py
+++ study/PyQt/spinbox/exercise.py
@@ -21,34 +21,19 @@ class WindowClass(QMainWindow, from_class):
 
         self.pushButton_2.clicked.connect(self.savefile)
         self.pushButton_3.clicked.connect(self.openfile)
-
-        self.height_bar.valueChanged.connect(self.resize_picture)
-        self.width_bar.valueChanged.connect(self.resize_picture)
-
-        # url = 'https://imageio.forbes.com/specials-images/imageserve/61b1f75e9bdd78e1c08fdd64/A-funny-labrador-dog-with-a-curiously-placed-bubble-in-its-behind-/0x0.jpg?format=jpg&crop=922,956,x0,y279,safe&width=1440'
-        # image = urllib.request.urlopen(url).read()
-        
         self.pixmap = QPixmap()
 
-
-    # def changeWidth(self):
-    #     width_value = self.width_bar.value()
-    #     self.pixmap = self.pixmap.scaledToWidth(width_value)
-    #     self.label_5.setPixmap(self.pixmap)
+        ## lambda를 쓰면 self.pixmap을 인자로 넣을 수 있다
+        self.height_bar.valueChanged.connect(lambda : self.resize_picture(self.pixmap))
+        self.width_bar.valueChanged.connect(lambda : self.resize_picture(self.pixmap))
         
-
-    # def changeHeight(self):
-    #     height_value = self.height_bar.value()
-    #     self.pixmap = self.pixmap.scaledToHeight(height_value)
-    #     self.label_5.setPixmap(self.pixmap)
-    
-    def resize_picture(self):
+    def resize_picture(self, pixmap):
         height_value = self.height_bar.value()
         width_value = self.width_bar.value()
         
-        self.pixmap = self.pixmap.scaled(width_value, height_value)
-        self.label_5.resize(self.pixmap.width(), self.pixmap.height())
-        self.label_5.setPixmap(self.pixmap)
+        pixmap = pixmap.scaled(width_value, height_value)
+        self.label_5.resize(pixmap.width(), pixmap.height())
+        self.label_5.setPixmap(pixmap)
 
     def savefile(self):
         
@@ -61,7 +46,6 @@ class WindowClass(QMainWindow, from_class):
     def openfile(self):
         path_name = QFileDialog.getOpenFileName(self, 'open file', './')
 
-        ## resize image to fit label size
         if path_name[0]:
             self.pixmap.load(path_name[0])
             self.pixmap = self.pixmap.scaled(self.label_5.width(), self.label_5.height(),)
