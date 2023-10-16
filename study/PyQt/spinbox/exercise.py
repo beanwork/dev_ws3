2e2cfa0ecb85586cb82c72eae587c3ea8c40ae63
beanwork <isumin138@gmail.com>
2023-10-16 18:19:52 +0900

    add resize

--- study/PyQt/spinbox/exercise.py
+++ study/PyQt/spinbox/exercise.py
@@ -11,23 +11,18 @@ class WindowClass(QMainWindow, from_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        min = self.spinBox.minimum()
-        max = self.spinBox.maximum()
-        step = self.spinBox.singleStep()
 
-        self.lineEdit.setText(str(min))
-        self.lineEdit_2.setText(str(max))
-        self.lineEdit_3.setText(str(step))
+        self.height.setRange(0, self.label_5.height())
+        self.height.setSingleStep(1)
 
-        self.horizontalSlider.setRange(min, max)
-        self.horizontalSlider.setSingleStep(step)
+        self.width.setRange(0, self.label_5.width())
+        self.width.setSingleStep(1)
 
-        self.pushButton.clicked.connect(self.apply)
         self.pushButton_2.clicked.connect(self.savefile)
         self.pushButton_3.clicked.connect(self.openfile)
 
-        self.spinBox.valueChanged.connect(self.changeSpinbox_and_changePixmapHeight)
-        self.horizontalSlider.valueChanged.connect(self.changeSlider_and_changePixmapWidth)
+        self.height.valueChanged.connect(self.resize)
+        self.width.valueChanged.connect(self.resize)
 
         url = 'https://imageio.forbes.com/specials-images/imageserve/61b1f75e9bdd78e1c08fdd64/A-funny-labrador-dog-with-a-curiously-placed-bubble-in-its-behind-/0x0.jpg?format=jpg&crop=922,956,x0,y279,safe&width=1440'
         image = urllib.request.urlopen(url).read()
@@ -38,25 +33,19 @@ class WindowClass(QMainWindow, from_class):
         self.pixmap = self.pixmap.scaled(self.label_5.width(), self.label_5.height())
         self.label_5.setPixmap(self.pixmap)
 
-    def apply(self):
-        min = self.lineEdit.text()
-        max = self.lineEdit_2.text()
-        step = self.lineEdit_3.text()
-
-        self.spinBox.setRange(int(min), int(max))
-        self.spinBox.setSingleStep(int(step))
-
-        self.horizontalSlider.setRange(int(min), int(max))
-        self.horizontalSlider.setSingleStep(int(step))
-    
-    def changeSlider_and_changePixmapWidth(self):
-        actualValue = self.horizontalSlider.value()
-        self.pixmap = self.pixmap.scaled(actualValue, self.label_5.height())
+    def changeWidth(self):
+        actualValue = self.width.value()
+        self.pixmap = self.pixmap.scaled(actualValue, actualValue)
         self.label_5.setPixmap(self.pixmap)
 
-    def changeSpinbox_and_changePixmapHeight(self):
-        actualValue = self.spinBox.value()
-        self.pixmap = self.pixmap.scaled(self.label_width(), actualValue)
+    def changeHeight(self):
+        actualValue = self.height.value()
+        self.pixmap = self.pixmap.scaled(self.label_5.height(), actualValue)
+        self.label_5.setPixmap(self.pixmap)
+    
+    def resize(self):
+        actualValue = self.height.value()
+        self.pixmap = self.pixmap.scaled(self.label_5.height(), actualValue)
         self.label_5.setPixmap(self.pixmap)
 
     def savefile(self):
