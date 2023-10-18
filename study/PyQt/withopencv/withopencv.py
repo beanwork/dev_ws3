a9d3631e626f15ef8815b6c71aecff60707f4a50
beanwork <isumin138@gmail.com>
2023-10-18 14:14:36 +0900

    add cameraon/off

--- study/PyQt/withopencv/withopencv.py
+++ study/PyQt/withopencv/withopencv.py
@@ -1,16 +1,49 @@
 import sys
+import cv2, imutils
+import time
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5 import uic
 from PyQt5.QtCore import *
 
-from_class = uic.loadUiType("exercise.ui")[0]
+
+from_class = uic.loadUiType("withopencv.ui")[0]
 
 class WindowClass(QMainWindow, from_class):
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.isCameraOn = False
+
+        self.pixmap = QPixmap()
+        self.open_File.clicked.connect(self.openFile)
+        self.camerabtn.clicked.connect(self.cameraOn)
+        
+    
+    def openFile(self):
+        file = QFileDialog.getOpenFileName(filter='Image (*.*)')
+
+        image = cv2.imread(file[0])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        h,w,c = image.shape
+        qimage = QImage(image.data, w, h, w*c, QImage.Format_RGB888)
+
+        self.pixmap = self.pixmap.fromImage(qimage)
+        self.pixmap = self.pixmap.scaled(self.label.width(), self.label.height())
+
+        self.label.setPixmap(self.pixmap)
+    
+    def cameraOn(self):
+        if self.isCameraOn == False:
+            self.camerabtn.setText('Camera Off')
+            self.isCameraOn = True
+        else:
+            self.camerabtn.setText('Camera On')
+            self.isCameraOn = False
+
+
 
 
 if __name__ == "__main__":
